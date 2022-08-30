41e097e9112f2c6bc63f345484cbef3d9058923c
Dssaenz <darwinsaenz105@gmail.com>
2022-08-30 12:22:02 -0500

    added styles for notes and main page

---
+++ src/journal/components/index.js
@@ -0,0 +1,6 @@
+import ImageGallery from './ImageGallery';
+import NavBar from './NavBar';
+import SideBar from './SideBar';
+import ButtonJournal from './ButtonJournal';
+
+export { ImageGallery, NavBar, SideBar, ButtonJournal };
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,16 @@
+import { ButtonJournal } from "../components";
+import JournalLayout from "../layout/JournalLayout";
+
+import { NoteView, NothingSelectedView } from "../../view";
+
 function JournalPage() {
   return (
-    <p>home</p>
-  )
+    <JournalLayout>
+      {/* <NothingSelectedView /> */}
+      <NoteView />
+      <ButtonJournal />
+    </JournalLayout>
+  );
 }
 
 export default JournalPage;
